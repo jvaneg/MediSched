153fa429fddee1560b72f84cdfed0ea808588dd9
arsh <arshdeep.dhillon1@ucalgary.ca>
2018-03-17 15:14:17 -0600

    Patient Search

---
+++ P3/Patients.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+namespace P3
+{
+    /// <summary>
+    /// Interaction logic for PatientInfo.xaml
+    /// </summary>
+    public partial class PatientInfo : Window
+    {
+        public PatientInfo()
+        {
+            InitializeComponent();
+        }
+
+
+        ObservableCollection<Patient> lstPatient = new ObservableCollection<Patient>();
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            lstPatient.Add(new Patient() { PatientNo = 1001, PatientName = "Mahesh" });
+            lstPatient.Add(new Patient() { PatientNo = 1002, PatientName = "Amit" });
+            lstPatient.Add(new Patient() { PatientNo = 1003, PatientName = "Vaibhav" });
+            lstPatient.Add(new Patient() { PatientNo = 1004, PatientName = "Ashwin" });
+            lstPatient.Add(new Patient() { PatientNo = 1005, PatientName = "Prashant" });
+            lstPatient.Add(new Patient() { PatientNo = 1006, PatientName = "Vinit" });
+            lstPatient.Add(new Patient() { PatientNo = 1007, PatientName = "Abhijit" });
+            lstPatient.Add(new Patient() { PatientNo = 1008, PatientName = "Pankaj" });
+            lstPatient.Add(new Patient() { PatientNo = 1009, PatientName = "Kaustubh" });
+            lstPatient.Add(new Patient() { PatientNo = 1010, PatientName = "Mohan" });
+
+            lstPatientData.ItemsSource = lstPatient;
+
+        }
+
+        private void txtNameToSearch_TextChanged(object sender,TextChangedEventArgs e)
+        {
+            string txtOrig = txtNameToSearch.Text;
+            string upper = txtOrig.ToUpper();
+            string lower = txtOrig.ToLower();
+
+            var empFiltered = from Emp in lstPatient let ename = Emp.PatientName
+                              where ename.StartsWith(lower) || ename.StartsWith(upper) || ename.Contains(txtOrig)
+                              select Emp;
+
+            lstPatientData.ItemsSource = empFiltered;
+        }
+    }
+}
